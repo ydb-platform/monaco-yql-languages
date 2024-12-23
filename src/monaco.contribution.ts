d9d06f97abbe5bf90a8e3a76c6fadea3e898c7f2
Anton Standrik <astandrik@gmail.com>
2024-12-23 15:32:27 +0300

    chore: export keywords (#29)

--- src/monaco.contribution.ts
+++ src/monaco.contribution.ts
@@ -3,3 +3,4 @@ import './s-expressions/s-expressions.contribution';
 import './themes/themes.contribution';
 import './yql/yql.contribution';
 import './yql/yql_ansi.contribution';
+import './yql/yql.keywords';
